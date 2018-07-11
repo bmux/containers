3c3e50c7c0a9d331663ff879398272a376313e93
bmux <sven.gradowski@bettermarks.com>
2018-07-11 12:49:16 +0200

    styles and examples added

--- js/project.js
+++ js/project.js
@@ -1,4 +1,4 @@
-$(".content-container-toggle").click(function () {
-  $(".content-container-body-wrapper").slideToggle();
-  $(".content-container").toggleClass("content-container-closed");
+$(".content-container-toggle-btn").click(function () {
+  $(this).parents(".content-container").children(".content-container-body-wrapper").slideToggle();
+  $(this).parents(".content-container").toggleClass("content-container-closed");
 });
